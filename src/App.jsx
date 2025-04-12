44c8baca72dba46db35fb79d3bc548338f7336f3
Yakoub <yakoubbenaissa@hotmail.com>
2025-04-12 11:17:16 +0100

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Tasks from './pages/Tasks';
-import EditTask from './pages/EditTask';
-import RegisterProject from './pages/RegisterProject';
-import ProtectedRoute from './components/ProtectedRoute';
+import Login from './components/forms/auth/Login';
+import Register from './components/forms/auth/Register';
 import useAuthStore from './store/authStore';
 
 function App() {
@@ -12,37 +9,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            isAuthenticated ? <Navigate to="/tasks" replace /> : <Navigate to="/login" replace />
-          }
-        />
+       
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/tasks"
-          element={
-            <ProtectedRoute>
-              <Tasks />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/tasks/edit/:id"
-          element={
-            <ProtectedRoute>
-              <EditTask />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/tasks/new"
-          element={
-            <ProtectedRoute>
-              <RegisterProject />
-            </ProtectedRoute>
-          }
-        />
+       
+       <Route path='/regiter' element={<Register/>}/>
       </Routes>
     </Router>
   );
